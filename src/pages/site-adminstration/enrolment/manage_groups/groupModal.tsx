27d88c2bc3c53facef37f78359ed3a44a9aa6ca8
alkama.azim <alkama.azim@ballisticlearning.com>
2023-05-02 11:03:29 +0530

    added manage group component with CRUD

---
+++ src/pages/site-adminstration/enrolment/manage_groups/groupModal.tsx
@@ -0,0 +1,174 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { Formik, Form } from "formik";
+import FieldLabel from "../../../../widgets/form_input_fields/labels";
+import FieldTypeText from "../../../../widgets/form_input_fields/form_text_field";
+import FieldErrorMessage from "../../../../widgets/form_input_fields/error_message";
+import FieldTypeTextarea from "../../../../widgets/form_input_fields/form_textarea_field";
+import CustomButton from "../../../../widgets/form_input_fields/buttons";
+import * as Yup from "yup";
+import { postData, putData } from "../../../../adapters/microservices";
+
+// Formik Yup validation === >>>
+const Schema = Yup.object({
+  name: Yup.string().min(1).required(),
+  // description: Yup.string().max(100).required(),
+});
+
+const GroupModal = ({
+  show,
+  onHide,
+  setModalShow,
+  courseid,
+  groupObj,
+  refreshGroupData,
+}: any) => {
+  // Initial values of react table === >>>
+  const initialValues = {
+    name: groupObj.name,
+    description: groupObj.description,
+  };
+
+  // custom Obj & handle form data === >>>
+  let formTitleHandle = {
+    btnTitle: "",
+    titleHeading: "",
+  };
+  if (groupObj.id === 0) {
+    formTitleHandle = {
+      btnTitle: "Save",
+      titleHeading: "Add Group",
+    };
+  } else {
+    formTitleHandle = {
+      btnTitle: "Update",
+      titleHeading: "Update Group",
+    };
+  }
+
+  const handleSubmit = (values: {}, { setSubmitting, resetForm }: any) => {
+    let endPoint = `/${courseid}/group`;
+    if (groupObj.id === 0) {
+      postData(endPoint, values)
+        .then((res: any) => {
+          if (res.data !== "" && res.status === 201) {
+            setModalShow(false);
+            refreshGroupData();
+            setSubmitting(false);
+            resetForm();
+          }
+        })
+        .catch((err: any) => {
+          console.log(err);
+          if (err.response.status === 404 || err.response.status === 400) {
+            if (err.response.data.userEmail !== undefined) {
+              window.alert(err.response.data.userEmail);
+            } else {
+              window.alert(err.response.data.message);
+            }
+          }
+        });
+    } else {
+      endPoint += `/${groupObj.id}`;
+      putData(endPoint, values)
+        .then((res: any) => {
+          if (res.data !== "" && res.status === 200) {
+            setModalShow(false);
+            refreshGroupData();
+            setSubmitting(false);
+          }
+        })
+        .catch((err: any) => {
+          console.log(err);
+          if (err.response.status === 404 || err.response.status === 400) {
+            if (err.response.data.userEmail !== undefined) {
+              window.alert(err.response.data.userEmail);
+            } else {
+              window.alert(err.response.data.message);
+            }
+          }
+        });
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
+        show={show}
+        onHide={onHide}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {formTitleHandle.titleHeading}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={Schema}
+            onSubmit={(values, action) => {
+              handleSubmit(values, action);
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form>
+                <div className="mb-3">
+                  <FieldLabel
+                    htmlfor="name"
+                    labelText="Name"
+                    required="required"
+                    star="*"
+                  />
+                  <FieldTypeText name="name" placeholder="Name" />
+                  <FieldErrorMessage
+                    errors={errors.name}
+                    touched={touched.name}
+                    msgText="Name required atleast 1 character"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <FieldLabel
+                    htmlfor="description"
+                    labelText="Description"
+                    // required="required"
+                    // star="*"
+                  />
+                  <FieldTypeTextarea
+                    name="description"
+                    component="textarea"
+                    placeholder="Description"
+                  />
+                  <FieldErrorMessage
+                    errors={errors.description}
+                    touched={touched.description}
+                    msgText="Please Enter description"
+                  />
+                </div>
+                <div className="text-center">
+                  <CustomButton
+                    type="submit"
+                    variant="primary"
+                    //   isSubmitting={isSubmitting}
+                    btnText={formTitleHandle.btnTitle}
+                  />{" "}
+                  {formTitleHandle.btnTitle === "Save" && (
+                    <CustomButton
+                      type="reset"
+                      btnText="Reset"
+                      variant="outline-secondary"
+                    />
+                  )}
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Modal.Body>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default GroupModal;
---
+++ src/pages/site-adminstration/enrolment/manage_groups/manageGroupTable.tsx
@@ -0,0 +1,141 @@
+import React, { useMemo } from "react";
+import Table from "react-bootstrap/Table";
+import { useTable } from "react-table";
+// import { Data } from "./rawData";
+import { Link } from "react-router-dom";
+import TableSkeleton from "../../../../widgets/skeleton/table";
+import { deleteData } from "../../../../adapters/microservices";
+
+// Actions btns styling === >>>
+const actionsStyle = {
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+};
+
+const ManageGroupTable = ({
+  manageGroupList,
+  courseid,
+  refreshOnDelete,
+  setModalShow,
+  editHandlerById,
+  refreshGroupData
+}: any) => {
+  const tableColumn = [
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Description",
+      accessor: "description",
+    },
+    {
+      Header: "Actions",
+      Cell: ({ row }: any) => (
+        <span style={actionsStyle}>
+          <Link to="">
+            <i
+              className="fa-solid fa-pen"
+              onClick={() =>
+                editHandler({
+                  id: row.original.id,
+                  name: row.original.name,
+                  description: row.original.description,
+                })
+              }
+            ></i>
+          </Link>
+          <Link to="">
+            <i
+              className="fa-solid fa-trash"
+              onClick={() => deleteHandler(row.original.id)}
+            ></i>
+          </Link>
+          <Link to="">
+            <i className="fa-solid fa-eye"></i>
+          </Link>
+        </span>
+      ),
+    },
+  ];
+
+  // react table custom variable decleration === >>>
+  const columns = useMemo(() => tableColumn, []);
+  const data = useMemo(() => manageGroupList, [manageGroupList]);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  // update event handler
+  const editHandler = ({id, name, description}: any) => {
+    setModalShow(true);
+    editHandlerById({id, name, description});
+    refreshGroupData()
+  };
+
+  // delete event handler === >>>
+  const deleteHandler = (id: number) => {
+    refreshOnDelete(false);
+    if (window.confirm("Are you sure to delete this department?")) {
+      let endPoint = `/${courseid}/group/${id}`;
+      deleteData(endPoint)
+        .then((res: any) => {
+          if (res.data !== "" && res.status === 200) {
+            refreshOnDelete(true);
+          } else if (res.status === 500) {
+            window.alert("Unable to delete, some error occurred");
+          }
+        })
+        .catch((result: any) => {
+          if (result.response.status === 400) {
+            window.alert(result.response.data.message);
+          } else {
+            window.alert(result.response.data.message);
+          }
+        });
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div className="table-wrapper mt-5">
+        <Table bordered hover {...getTableProps}>
+          <thead>
+            {headerGroups.map((headerGroup, index) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+                {headerGroup.headers.map((column, index) => (
+                  <th {...column.getHeaderProps()} key={index}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+
+          <tbody {...getTableBodyProps}>
+            {rows.map((row, index) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={index}>
+                  {row.cells.map((cell, index) => (
+                    <td {...cell.getCellProps()} key={index}>
+                      {cell.render("Cell")}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+        {manageGroupList.length === 0 && (
+          <TableSkeleton numberOfRows={5} numberOfColumns={4} />
+        )}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ManageGroupTable;
