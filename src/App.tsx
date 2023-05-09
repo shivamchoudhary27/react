c3dda647b3701eb36117704f77618a049d22dfb0
alkama.azim <alkama.azim@ballisticlearning.com>
2023-05-09 13:16:00 +0530

    restructuring router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomRoutes from './features/routes/routes';
+// import CustomRoutes from './features/routes/routes';
 import { UserContextProvider } from './features/context/user/user';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
@@ -7,11 +7,12 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import './assets/styles/global.scss';
 // import './widgets/components.css';
+import NewCustomRoutes from './features/routes';
 
 function App() {
   return (
     <UserContextProvider>
-      <CustomRoutes />
+      <NewCustomRoutes />
     </UserContextProvider>
   );
 }
