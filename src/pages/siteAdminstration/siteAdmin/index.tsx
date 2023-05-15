09d1f96d73f9c20f86e1b9a06d2b9ec7ae29b965
Amit Singh <amit.singh@ballisticlearning.com>
2023-05-15 13:26:27 +0530

    Changes for site admin landing page box hover animation.

--- src/pages/siteAdminstration/siteAdmin/index.tsx
+++ src/pages/siteAdminstration/siteAdmin/index.tsx
@@ -14,18 +14,18 @@ const SiteAdminHome = () => {
       <Header />
       <HeaderTabs />
       <div className="contentarea-wrapper mt-5">
-        <Container className="administration-box">
-          <Row>
-            {AdminRawData.map((item, index) => (
-              <div key={index} className="col-4 mb-4">
+      {AdminRawData.map((item, index) => (
+        <Container key={index} className={`administration-box row${index + 1}`}>
+            {item.map((item, index) => (
+              <div key={index} className={`box ${item.boxclassname}`}>
                 <Link to={item.link} className={`default-item ${item.classname}`}>
                     <h4 className="card-title" dangerouslySetInnerHTML={{ __html: item.title }} />
                     <img src={item.image} alt={item.title} className="img-fluid" />                    
                 </Link>
               </div>
             ))}
-          </Row>
         </Container>
+        ))}
       </div>
       <Footer />
     </React.Fragment>
--- src/pages/siteAdminstration/siteAdmin/rawData.tsx
+++ src/pages/siteAdminstration/siteAdmin/rawData.tsx
@@ -5,40 +5,51 @@ import copomanagement from '../../../assets/images/copo-management.png';
 import timetablemanagement from '../../../assets/images/timetable-management.png';
 import calendaermanagement from '../../../assets/images/timetable-management.png';
 export const AdminRawData = [
-  {
-    title: "User <br>Management",
-    image: usermanagement,
-    link: "/usermanagement",
-    classname: "user-management",
-  },
-  {
-    title: "Program <br>Management",
-    image: manageprogram,
-    link: "/manageprogram",
-    classname: "manage-program",
-  },
-  {
-    title: "Program <br>Enrollment",
-    image: programenrollment,
-    link: "/programenrollment",
-    classname: "program-enrollment",
-  },  
-  {
-    title: "CO's / PO's <br>Management",
-    image: copomanagement,
-    link: "#",
-    classname: "copo-management",
-  },
-  {
-    title: "Timetable <br>Management",
-    image: timetablemanagement,
-    link: "#",
-    classname: "timetable-management",
-  },
-  {
-    title: "Calendar <br>Management",
-    image: calendaermanagement,
-    link: "/calenderconfig",
-    classname: "calender-config",
-  }, 
+  [
+    {
+      title: "User <br>Management",
+      image: usermanagement,
+      link: "/usermanagement",
+      classname: "user-management",
+      boxclassname: "",
+    },
+    {
+      title: "Program <br>Management",
+      image: manageprogram,
+      link: "/manageprogram",
+      classname: "manage-program",
+      boxclassname: "middle",
+    },
+    {
+      title: "Program <br>Enrollment",
+      image: programenrollment,
+      link: "/programenrollment",
+      classname: "program-enrollment",
+      boxclassname: "",
+    },
+  ],
+
+  [
+    {
+      title: "CO's / PO's <br>Management",
+      image: copomanagement,
+      link: "#",
+      classname: "copo-management",
+      boxclassname: "row2box",    
+    },
+    {
+      title: "Timetable <br>Management",
+      image: timetablemanagement,
+      link: "#",
+      classname: "timetable-management",
+      boxclassname: "",
+    },
+    {
+      title: "Calendar <br>Management",
+      image: calendaermanagement,
+      link: "/calenderconfig",
+      classname: "calender-config",
+      boxclassname: "row2box",
+    },
+  ]
 ];
