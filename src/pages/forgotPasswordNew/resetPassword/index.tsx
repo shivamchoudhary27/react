2f9715ad8b6bab495e29841c4af13c21657d73dc
alkama.azim <alkama.azim@ballisticlearning.com>
2023-06-02 13:23:39 +0530

    added alert in signup form

--- src/pages/forgotPasswordNew/resetPassword/index.tsx
+++ src/pages/forgotPasswordNew/resetPassword/index.tsx
@@ -1,7 +1,7 @@
-import React , {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Alert from 'react-bootstrap/Alert';
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 import { Formik, Form } from "formik";
 import FieldLabel from "../../../widgets/formInputFields/labels";
 import FieldTypeText from "../../../widgets/formInputFields/formTextField";
@@ -9,27 +9,29 @@ import FieldErrorMessage from "../../../widgets/formInputFields/errorMessage";
 import { putData } from "../../../adapters/coreservices";
 import config from "../../../utils/config";
 import * as Yup from "yup";
+import Errordiv from "../../../widgets/alert/errordiv";
 
 const initialValues = {
-    newPassword: "",
-    confirmPassword: "",
+  newPassword: "",
+  confirmPassword: "",
 };
 
 const ResetPasswordForm = () => {
   const location = useLocation().search;
   const [requestToken, setRequestToken] = useState<string | null>(null);
-  const [signInLink, setSigninLink] = useState<boolean>(false);
   const redirectUri = config.REDIRECT_URI;
   const oAuthUrl = `${config.OAUTH2_URL}/authorize?response_type=code&client_id=moodle&redirect_uri=${redirectUri}&scope=openid`;
+  const [alertStatus, setAlertStatus] = useState(false);
+  const [alertMsg, setAlertMsg] = useState({ message: "", alertBoxColor: "" });
 
   // Formik Yup validation === >>>
   const userFormSchema = Yup.object({
     newPassword: Yup.string().required(),
     confirmPassword: Yup.string()
-    .required()
-    .test('passwords-match', 'Passwords must match', function (value) {
-      return value === this.parent.newPassword;
-    }),
+      .required()
+      .test("passwords-match", "Passwords must match", function (value) {
+        return value === this.parent.newPassword;
+      }),
   });
 
   useEffect(() => {
@@ -40,83 +42,106 @@ const ResetPasswordForm = () => {
   // handle Form CRUD operations === >>>
   const handleFormData = (values: any, { setSubmitting, resetForm }: any) => {
     // values.token = urlParams;
-    let endPoint = `/user/resetPassword?token=${requestToken}`
+    let endPoint = `/user/resetPassword?token=${requestToken}`;
     putData(endPoint, values)
-    .then((res: any) => {
-      console.log('resetpassowrd', res)
-      if (res.status === 200) {
-        setSigninLink(true);
-      } else {
-        window.alert('Some error occurred');
-      }
-    })
-    .catch((err: any) => {
-      console.log('error', err);
-      window.alert('Some error occurred');
-    });
+      .then((res: any) => {
+        console.log("resetpassowrd", res);
+        if (res.status === 200) {
+          setAlertStatus(true);
+          setAlertMsg({
+            message: "Password reset successfully.",
+            alertBoxColor: "",
+          });
+        } else {
+          // window.alert("Some error occurred");
+          setAlertStatus(true);
+          setAlertMsg({
+            message: "Some error occurred. Try again!",
+            alertBoxColor: "alert-danger",
+          });
+        }
+      })
+      .catch((err: any) => {
+        // console.log("error", err);
+        // window.alert("Some error occurred");
+        setAlertStatus(true);
+          setAlertMsg({
+            message: "Some error occurred. Try again!",
+            alertBoxColor: "alert-danger",
+          });
+      });
   };
 
   return (
     <React.Fragment>
-        <Formik
-          enableReinitialize={true}
-          initialValues={initialValues}
-          validationSchema={userFormSchema}
-          onSubmit={(values, action) => {
-            handleFormData(values, action)
-          }}
-        >
-          {({ errors, touched, isSubmitting, setValues, values }) => (
-            <Form className="row">
-              <div className="col-12 mb-4 text-start">
-                <FieldLabel
-                  htmlfor="newPassword"
-                  labelText="New Password"
-                  required="required"
-                  className="form-label"
-                />
-                <FieldTypeText type="password" name="newPassword" placeholder="New Password" />
-                <FieldErrorMessage
-                  errors={errors.newPassword}
-                  touched={touched.newPassword}
-                  msgText="Please enter a new password"
-                />
-              </div>              
-              <div className="col-12 mb-4 text-start">
-                <FieldLabel
-                  htmlfor="confirmPassword"
-                  labelText="Confirm Password"
-                  required="required"
-                  className="form-label"
-                />
-                <FieldTypeText type="password" name="confirmPassword" placeholder="Confirm Password" />
-                <FieldErrorMessage
-                  errors={errors.confirmPassword}
-                  touched={touched.confirmPassword}
-                  msgText="Please re-enter password that must match new password"
-                />
-              </div>              
-              <div className="col-12 mb-4 d-grid">
-                <Button
-                  type="submit"
-                  variant="primary"
-                >
-                  Submit
-                </Button>
-                {" "}
-              </div>
-            </Form>            
-          )}
-        </Formik>
-        {
-          signInLink &&
-          <React.Fragment>
-            <Alert variant="success">
-              <h5>Password reset successfully</h5>
-            </Alert>
-            <p>Go to <a href={oAuthUrl} className="ms-1">Sign in</a></p>
-          </React.Fragment>
-        }
+      {alertStatus === true && (
+        <Errordiv
+          msg={alertMsg.message}
+          cstate
+          className="mt-3"
+          alertColor={alertMsg.alertBoxColor}
+        />
+      )}
+      <Formik
+        enableReinitialize={true}
+        initialValues={initialValues}
+        validationSchema={userFormSchema}
+        onSubmit={(values, action) => {
+          handleFormData(values, action);
+        }}
+      >
+        {({ errors, touched, isSubmitting, setValues, values }) => (
+          <Form className="row">
+            <div className="col-12 mb-4 text-start">
+              <FieldLabel
+                htmlfor="newPassword"
+                labelText="New Password"
+                required="required"
+                className="form-label"
+              />
+              <FieldTypeText
+                type="password"
+                name="newPassword"
+                placeholder="New Password"
+              />
+              <FieldErrorMessage
+                errors={errors.newPassword}
+                touched={touched.newPassword}
+                msgText="Please enter a new password"
+              />
+            </div>
+            <div className="col-12 mb-4 text-start">
+              <FieldLabel
+                htmlfor="confirmPassword"
+                labelText="Confirm Password"
+                required="required"
+                className="form-label"
+              />
+              <FieldTypeText
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+              />
+              <FieldErrorMessage
+                errors={errors.confirmPassword}
+                touched={touched.confirmPassword}
+                msgText="Please re-enter password that must match new password"
+              />
+            </div>
+            <div className="col-12 mb-4 d-grid">
+              <Button type="submit" variant="primary">
+                Submit
+              </Button>{" "}
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <p>
+        Go to{" "}
+        <a href={oAuthUrl} className="ms-1">
+          Sign in
+        </a>
+      </p>
     </React.Fragment>
   );
 };
--- src/pages/signupNew/form/index.tsx
+++ src/pages/signupNew/form/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Formik, Form } from "formik";
 import FieldLabel from "../../../widgets/formInputFields/labels";
@@ -11,16 +11,19 @@ import { postData } from "../../../adapters/coreservices";
 import * as Yup from "yup";
 import ReCAPTCHA from "react-google-recaptcha";
 import googleReCaptcha from "../../../utils/recaptcha";
+import Errordiv from "../../../widgets/alert/errordiv";
 
 const SignupForm = () => {
   const navigate = useNavigate();
+  const [alertStatus, setAlertStatus] = useState(false);
+  const [alertMsg, setAlertMsg] = useState({ message: "", alertBoxColor: "" });
 
   const initialValues = {
     lastName: "",
     firstName: "",
     email: "",
     country: "",
-    recaptcha:""
+    recaptcha: "",
   };
   // Formik Yup validation === >>>
   const userFormSchema = Yup.object({
@@ -28,48 +31,83 @@ const SignupForm = () => {
     firstName: Yup.string().min(1).trim().required(),
     lastName: Yup.string().min(1).trim().required(),
     country: Yup.string().required(),
-    recaptcha: Yup.string().required()
+    recaptcha: Yup.string().required(),
   });
 
   // handle Form CRUD operations === >>>
-  const handleFormData = (values: any, { setSubmitting, resetForm, setFieldError }: any) => {
+  const handleFormData = (
+    values: any,
+    { setSubmitting, resetForm, setFieldError }: any
+  ) => {
     console.log(values);
     values.idnumber = 98789871;
     values.city = "Delhi";
-    // if(values.recaptcha !== ""){
+    if (values.recaptcha !== "") {
       postData("/user/signup", values)
         .then((res: any) => {
           console.log("res", res);
           if (res.status === 201 || res.status === 200) {
-            window.alert("Registration successful");
+            setAlertStatus(true);
+            setAlertMsg({
+              message:
+                "Form submit successfully, Link send to your email for set password.",
+              alertBoxColor: "",
+            });
           } else {
             window.alert("Some error occurred");
           }
-          navigate("/");
+          // setTimeout(()=>{
+          //   navigate("/");
+          // }, 3000)
         })
         .catch((err: any) => {
           if (err.response.status === 404) {
-            window.alert(err.response.data.message);
+            // window.alert(err.response.data.message);
+            setAlertStatus(true);
+            setAlertMsg({
+              message: "User already exists. Please Sign in",
+              alertBoxColor: "alert-warning"
+            });
           } else {
-            window.alert("Some error occurred");
+            // window.alert("Some error occurred");
+            setAlertStatus(true);
+            setAlertMsg({
+              message: "Some error occurred, Try again!",
+              alertBoxColor: "alert-danger"
+            });
           }
-        });    
-    // }
+        });
+    }
   };
 
   return (
     <React.Fragment>
+      {alertStatus === true && (
+        <Errordiv
+          msg={alertMsg.message}
+          cstate
+          className="mt-3"
+          alertColor={alertMsg.alertBoxColor}
+        />
+      )}
       <Formik
         enableReinitialize={true}
         initialValues={initialValues}
         validationSchema={userFormSchema}
         onSubmit={(values, action) => {
-          console.log(values)
+          console.log(values);
           handleFormData(values, action);
         }}
       >
-        {({ errors, touched, isSubmitting, setValues, values, setFieldValue }) => (
-          <Form className="row">                
+        {({
+          errors,
+          touched,
+          isSubmitting,
+          setValues,
+          values,
+          setFieldValue,
+        }) => (
+          <Form className="row">
             <div className="col-lg-6 mb-3 text-start">
               <FieldLabel
                 htmlfor="firstName"
@@ -137,7 +175,7 @@ const SignupForm = () => {
             <div className="mb-4">
               <ReCAPTCHA
                 sitekey={googleReCaptcha.SITE_KEY}
-                onChange={(value: any) => setFieldValue('recaptcha', value)}
+                onChange={(value: any) => setFieldValue("recaptcha", value)}
               />
               <FieldErrorMessage
                 errors={errors.recaptcha}
