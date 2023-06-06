fa2658ad5a20b792f2f33413f7cc25cec40e8329
Amit Singh <amit.singh@ballisticlearning.com>
2023-06-06 13:24:47 +0530

    Changes for institute filter on manage program pages.

--- src/pages/siteAdminstration/discipline/index.tsx
+++ src/pages/siteAdminstration/discipline/index.tsx
@@ -143,7 +143,14 @@ const Discipline = () => {
       <div className="contentarea-wrapper mt-3">
         <Container fluid>
           <PageTitle pageTitle="Discipline" gobacklink="/manageprogram" />
-          Institute : <InstituteFilter updateCurrentInstitute={updateCurrentInstitute}/>
+          <div className="row gx-2 mb-3 align-items-center justify-content-center">
+            <div className="col-auto">
+              <label className="col-form-label">Institute</label>
+            </div>
+            <div className="col-auto">
+              <InstituteFilter updateCurrentInstitute={updateCurrentInstitute}/>
+            </div>
+          </div>
           <Filters
             openAddDiscipline={openAddDiscipline}
           />
