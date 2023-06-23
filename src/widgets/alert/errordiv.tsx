8c4589fcdd49b6038c6cd370e97332f9a4cc1dcb
Amit Singh <amit.singh@ballisticlearning.com>
2023-06-23 12:05:33 +0530

    Changes for alert component.

--- src/widgets/alert/errordiv.tsx
+++ src/widgets/alert/errordiv.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import Alert from "react-bootstrap/Alert";
 
-function Errordiv({ cstate, msg, className, alertColor = "alert-primary" }: any) {
+function Errordiv({ cstate, msg, className='', variant = "primary" }: any) {
   const [show, setShow] = useState(cstate);
   return (
     <>
-      <div className="table-responsive table-wrapper mt-3">
-        {show === true && (
-          <Alert
-            className={`${className} alert ${alertColor}`}
-            role="alert"
-            onClose={() => setShow(false)}
-          >
-            {msg}
-          </Alert>
-        )}
-      </div>
+      {show === true && (
+        <Alert
+          className={`${className}`}
+          role="alert"
+          variant={`${variant}`}
+          onClose={() => setShow(false)}
+        >
+          {msg}
+        </Alert>
+      )}
     </>
   );
 }
