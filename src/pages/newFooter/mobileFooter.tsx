03c69fc2129fe5d4b28646b9604d08613ae7fa2c
vijay dhyani <vijay.dhyani@ballisticlearning.com>
2023-08-11 13:30:25 +0530

    Header/Footer & Mobile Sidebar UI added

--- src/pages/newFooter/mobileFooter.tsx
+++ src/pages/newFooter/mobileFooter.tsx
@@ -1,31 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+// this icon will be used later for inactive state
+// import DashboardIcon from "../../assets/images/icons/mb-dashboard-icon.svg";
+import DashboardIcon from "../../assets/images/icons/mb-dashboard-icon-active.svg";
+import CalenderIcon from "../../assets/images/icons/mb-calender-icon.svg";
+import GradebookIcon from "../../assets/images/icons/mb-gradebook-icon.svg";
+import PerformanceIcon from "../../assets/images/icons/mb-performance-icon.svg";
+import MenuIcon from "../../assets/images/icons/mb-menu-icon.svg";
+import "./mobileStyle.scss";
+import Sidebar from "./sidebar";
 type Props = {};
 
 const MobileFooter = (props: Props) => {
+  const location = useLocation();
+
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
+  const closeSidebar = () => {
+    setSidebarVisible(false);
+  };
+
   return (
-    <React.Fragment>
-      <div style={{ textAlign: "center" }}>
-        <ul
-          style={{
-            paddingLeft: "0px",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+    <footer className="md-stickyFooter">
+      <div>
+        <ul className="d-flex justify-content-between align-items-center p-0 m-0">
           {footerList.map((list, index) => (
             <Link to={list.link}>
-              <li key={index} style={{ listStyle: "none" }}>
-                {list.icon}
-                <p>{list.title}</p>
+              <li
+                className={`text-center list-unstyled md-footer-list ${
+                  location.pathname === list.link ? "active" : ""
+                }`}
+                onClick={list.title === "Menu" ? toggleSidebar : undefined}
+              >
+                <img src={list.icon} alt={list.title} />
+                <h2
+                  className={`footerTitle ${
+                    location.pathname === list.link ? "active" : ""
+                  }`}
+                >
+                  {list.title}
+                </h2>
               </li>
             </Link>
           ))}
         </ul>
       </div>
-    </React.Fragment>
+      {isSidebarVisible && <Sidebar onClose={closeSidebar} />}
+    </footer>
   );
 };
 
@@ -33,27 +57,27 @@ export default MobileFooter;
 
 const footerList = [
   {
-    icon: <i className="fa-solid fa-gauge"></i>,
+    icon: DashboardIcon,
     title: "Dashboard",
-    link: "#",
+    link: "/studentdashboard",
   },
   {
-    icon: <i className="fa-solid fa-calendar-days"></i>,
+    icon: CalenderIcon,
     title: "Calander",
     link: "#",
   },
   {
-    icon: <i className="fa-solid fa-calendar-days"></i>,
+    icon: GradebookIcon,
     title: "Gradebook",
     link: "#",
   },
   {
-    icon: <i className="fa-solid fa-calendar-days"></i>,
+    icon: PerformanceIcon,
     title: "Performance",
     link: "#",
   },
   {
-    icon: <i className="fa-solid fa-bars"></i>,
+    icon: MenuIcon,
     title: "Menu",
     link: "#",
   },
