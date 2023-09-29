1675b4ac4d76acdb59ded1bbf1cc79223833d678
Amit Singh <amit.singh@ballisticlearning.com>
2023-09-29 17:20:08 +0530

    Changes for user profile page.

--- src/pages/user/profile/index.tsx
+++ src/pages/user/profile/index.tsx
@@ -10,14 +10,15 @@ import PageTitle from "../../../widgets/pageTitle";
 import { searchCountryNameById } from "../../../globals/getCountry";
 import EditUserProfile from "./modal";
 import { Button } from "react-bootstrap";
+import "./style.scss";
 
 const UserProfile = () => {
     const [user, setUser] = useState({
-        "userFirstName": "",
-        "userLastName": "",
-        "userEmail": "",
+        "userFirstName": "--",
+        "userLastName": "--",
+        "userEmail": "--",
         "userId": 0,
-        "userCountry": "",
+        "userCountry": "--",
         "enabled": false,
         "roles": [],
         "files": []
@@ -58,24 +59,78 @@ const UserProfile = () => {
             <div className="contentarea-wrapper mt-3 mb-5">
                 <Container fluid>
                     <PageTitle
-                        pageTitle={(user.userFirstName + ' ' + user.userLastName).replace(/\b\w/g, match => match.toUpperCase())} 
+                        pageTitle={""} 
                         gobacklink="/dashboard" 
                     />
-                    <h3>Country: {searchCountryNameById(user.userCountry)} </h3>
-                    <h3>Email: {user.userEmail} </h3>
-                    {
-                        user.roles.length > 0 &&
-                        <h3>Roles: 'User Roles' </h3>
-                    }
-                    <Button onClick={() => toggleModalShow('profile')}>Edit Profile</Button>
-                    <div>
-                        <img
-                            onClick={() => toggleModalShow('picture')}
-                            className="img-fluid"
-                            src={user.files.length > 0 ? user.files[0].url : ''}
-                            alt={user.files.length > 0 ? user.files[0].originalFileName : user.userFirstName}
-                            width={"500px"}
-                        />
+                    <div className="user-profile-box">
+                        <div className="row">
+                            <div className="col-md-4 text-center">
+                                <div className="user-picture">
+                                    <img
+                                        onClick={() => toggleModalShow('picture')}
+                                        src={user.files.length > 0 ? user.files[0].url : ''}
+                                        alt={user.files.length > 0 ? user.files[0].originalFileName : user.userFirstName}
+                                    />
+                                </div>
+                                <h3 className="mt-3">
+                                    {(user.userFirstName + ' ' + user.userLastName).replace(/\b\w/g, match => match.toUpperCase())}
+                                </h3>
+                                <Button onClick={() => toggleModalShow('profile')}>Edit Profile</Button>
+                            </div>
+
+                            <div className="col-md-8">                                    
+                                <ul className="profile-menu-data">
+                                    <li>
+                                        <label>First Name</label>
+                                        {user.userFirstName}
+                                    </li>
+                                    <li>
+                                        <label>Last Name</label>
+                                        {user.userLastName}            
+                                    </li>
+                                    <li>
+                                        <label>Email</label>
+                                        {user.userEmail}
+                                    </li>      
+                                    <li>
+                                        <label>Mobile Number</label>
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Gender</label>
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Date of Birth</label>
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Country</label>
+                                        {searchCountryNameById(user.userCountry)}
+                                    </li>                                    
+                                    <li>
+                                        <label>Blood Group</label>
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Father Name</label> 
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Mother Name</label> 
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Parents Mobile No</label> 
+                                        --
+                                    </li>
+                                    <li>
+                                        <label>Parents Email Id</label> 
+                                        --
+                                    </li>    
+                                </ul>
+                            </div>
+                        </div>   
                     </div>
                 </Container>
             </div>
