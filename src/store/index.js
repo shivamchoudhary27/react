25d00b448c8c88ac44e4a3d1993e03c1433becfa
abhijit.singh <abhijit.singh@ballisticlearing.com>
2023-10-06 12:59:53 +0530

    user profile info save to new redux slice

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,15 @@ import userAuthoritiesSlice from './slices/userRoles';
 import globalFilterSlice from './slices/globalFilters';
 import globalAlertSlice from './slices/globalAlerts';
 import globalUserInfo from './slices/userInfo';
+import globalUserProfile from './slices/userProfile';
 
 const store = configureStore({
     reducer: {
         globalFilters: globalFilterSlice.reducer,
         globalAlerts: globalAlertSlice.reducer,
         userAuthorities: userAuthoritiesSlice.reducer,
-        userInfo: globalUserInfo.reducer
+        userInfo: globalUserInfo.reducer,
+        userProfile: globalUserProfile.reducer
     }
 });
 
