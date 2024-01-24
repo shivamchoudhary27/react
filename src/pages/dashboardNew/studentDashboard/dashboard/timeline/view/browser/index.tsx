d0957da70f48a0d1d7b37777079c9d0ad19f104b
Akshay Sharma <akshay.sharma@ballisticlearning.com>
2024-01-24 11:50:34 +0530

    dashbord timeLine filter

--- src/pages/dashboardNew/studentDashboard/dashboard/timeline/view/browser/index.tsx
+++ src/pages/dashboardNew/studentDashboard/dashboard/timeline/view/browser/index.tsx
@@ -4,17 +4,6 @@ import ListSkeleton from "../../../../../../../widgets/skeleton/list";
 import { Link } from "react-router-dom";
 
 type Props = {
-  // eventsPacket: any;
-  // apiStatus: string;
-  // showAlert: boolean;
-  // getTimetableTime: any;
-  // getFilterSelectValue: any;
-  // formatDynamicTimestamp: any;
-  // filterTimestampValue: string;
-  // getSortFilterValue: any;
-  // filterTimestampSort: string;
-  // courseFilterActive: boolean;
-
   eventsPacket: any;
   apiStatus: string;
   showAlert: boolean;
@@ -84,53 +73,41 @@ const Browser: React.FC<Props> = (props) => {
     <div className="mitblock activityTimeline-block">
       <h3 className="mitblock-title">Activity Timeline</h3>
       <FilterDropdown
-        // eventsPacket={props.eventsPacket}
-        // getFilterSelectValue={props.getFilterSelectValue}
-        // filterTimestampValue={props.filterTimestampValue}
-        // getSortFilterValue={props.getSortFilterValue}
-        // filterTimestampSort={props.filterTimestampSort}
-
+      
         eventsPacket={props.eventsPacket}
         getFilterSelectValue={props.getFilterSelectValue}
         filterTimestampValue={props.filterTimestampValue}
         getSortFilterValue={props.getSortFilterValue}
         filterTimestampSort={props.filterTimestampSort}
       />
-      <div className="mitblock-body">
-        {props.eventsPacket.length > 0 &&
-          props.eventsPacket.map((event: any, index: number) => (
+        <div className="mitblock-body">
+      {props.eventsPacket.length > 0 &&
+        props.eventsPacket.map((event: any, index: number) =>
+          props.filterTimestampSort !== "course" ? (
             <div className="d-flex align-items-center atb-row" key={index}>
               {/* <div className="align-self-start me-3"><img src={el.course.courseimage} alt="" /></div> */}
               <div className="atb-info">
-                {props.courseFilterActive !== false && (
-                  <h6 style={{ fontSize: "18px", fontWeight: "700" }}>
-                    {event.course.fullname}
-                  </h6>
-                )}
                 <h6>
                   <a href={event.viewurl} className="me-2">
                     {formatDynamicTimestamp(event.timesort)}
                   </a>
                   {event.timesort < currentTimestamp && (
-                  <span
-                    style={{
-                      padding: "3px",
-                      fontSize: "11px",
-                      fontWeight: 410,
-                      borderRadius: "10px",
-                      backgroundColor: "#FF8989",
-                      display: "inline-block",
-                    }}
-                  >
-                    Overdue
-                  </span>
-                )}
+                    <span
+                      style={{
+                        padding: "3px",
+                        fontSize: "11px",
+                        fontWeight: 410,
+                        borderRadius: "10px",
+                        backgroundColor: "#FF8989",
+                        display: "inline-block",
+                      }}
+                    >
+                      Overdue
+                    </span>
+                  )}
                 </h6>
                 <p>
-                  {event.name}.{" "}
-                  {props.courseFilterActive !== true && (
-                    <b>{event.course.fullname}</b>
-                  )}
+                  {event.name}. <b>{event.course.fullname}</b>
                 </p>
                 <p>{getTimetableTime(event.timestart)}</p>
               </div>
@@ -141,8 +118,47 @@ const Browser: React.FC<Props> = (props) => {
                 {event.action.name}
               </Link>
             </div>
-          ))}
-
+          ) : (
+            <div className="d-flex align-items-center atb-row" key={index}>
+              {/* <div className="align-self-start me-3"><img src={el.course.courseimage} alt="" /></div> */}
+              <div className="atb-info">
+                <h6>
+                  <a href={event.viewurl} className="me-2">
+                    {event.course.fullname}
+                  </a>
+                  {event.timesort < currentTimestamp && (
+                    <span
+                      style={{
+                        padding: "3px",
+                        fontSize: "11px",
+                        fontWeight: 410,
+                        borderRadius: "10px",
+                        backgroundColor: "#FF8989",
+                        display: "inline-block",
+                      }}
+                    >
+                      Overdue
+                    </span>
+                  )}
+                  <p>
+                    {event.name}
+                    {".  "}
+                  </p>
+                </h6>
+                <p>
+                  <b>{formatDynamicTimestamp(event.timesort)}</b>
+                </p>
+                <p>{getTimetableTime(event.timestart)}</p>
+              </div>
+              <Link
+                to={event.action.url}
+                className="btn btn-light btn-sm atb-button"
+              >
+                {event.action.name}
+              </Link>
+            </div>
+          )
+        )}
         {props.apiStatus === "started" && props.eventsPacket.length === 0 && (
           <ListSkeleton />
         )}
