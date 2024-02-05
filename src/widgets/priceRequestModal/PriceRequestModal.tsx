8d961a43fb7684d4d01b5f7b7eb65bd7ea1df4d5
vijay.dhyani <vijay.dhyani@ballisticlearning.com>
2024-02-05 18:13:11 +0530

    request modal

--- src/widgets/priceRequestModal/PriceRequestModal.tsx
+++ src/widgets/priceRequestModal/PriceRequestModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import RequestImage from "../../assets/images/price-request.png";
+import MailIcon from "../../assets/images/icons/mail.svg";
+import WaveBottom from "../../assets/images/background/bg-modal.svg";
 import "./style.scss";
 
 const PriceRequestModal = ({ show, handleClose }) => {
@@ -12,14 +14,17 @@ const PriceRequestModal = ({ show, handleClose }) => {
       <div className="d-flex p-2 align-items-center gap-5">
         <div className="d-flex flex-column gap-2 requestmodal-content">
             <h4>Price Upon Request</h4>
-            <input type="text" placeholder="Enter Your Email" />
-            <Button variant="primary">Submit</Button>
+            <div className="d-flex flex-row gap-2">
+              <img src={MailIcon} alt="Mail" className="img img-fluid" />
+              <a href="mailto:enquiry@ballisticlearning.com">enquiry@ballisticlearning.com</a>
+            </div>
         </div>
         <div className="request-image">
-            <img src={RequestImage} alt="Price Request" />
+            <img src={RequestImage} alt="Price Request"/>
         </div>
       </div>
       </Modal.Body>
+      <img src={WaveBottom} alt="WaveBottom" className="wavebg"/>
     </Modal>
   );
 };
