add02b820e8a2b005fd3d9f3d33c0a4cf92a807b
alkama.azim <alkama.azim@ballisticlearning.com>
2024-02-14 10:12:19 +0530

    added logout functionality

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../features/context/user/user";
 
-type Props = {}
+type Props = {};
 
 const Logout = (props: Props) => {
-    const navigate = useNavigate()
-    useEffect(() => {
-      console.log("Navigated to Logout Page");
-      // localStorage.clear();
-      //   navigate('/')
-    }, [])
+  const userCtx = useContext(UserContext)
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log("Navigated to Logout Page");
+    console.log(userCtx.token)
+    console.log(userCtx.isLoggedIn);
+    localStorage.clear();
+    navigate("/");
+  }, []);
 
-  return (
-    <div>Logout</div>
-  )
-}
+  return <></>;
+};
 
-export default Logout
+export default Logout;
