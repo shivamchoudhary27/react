a99729a9c0c0191d903a9c4ea45d757cf6ce2c67
krisha.singh <krisha.singh@ballisticlearning.com>
2024-02-15 22:56:29 +0530

    added program preview for landing page

---
+++ src/pages/home/preview/ratings/ratings.tsx
@@ -0,0 +1,93 @@
+import "./style.scss";
+import axios from "axios";
+import Review from "./review";
+import { Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import StarRating from "../../../../widgets/rating";
+
+interface IProps {
+  programid: any
+} 
+
+const calculateNetRating = (ratingArray: number[]) => {
+  const total = ratingArray.reduce((accumulator : number, currentValue: number) => accumulator + currentValue, 0);
+  return (total / ratingArray.length).toFixed(1);
+}
+
+const roundToWhole = (number : number) => {
+  return ((number % 1) >= 0.5) ? Math.ceil(number) : Math.floor(number);
+}
+
+const programRatingTemplate = {
+  ratingId: null,
+  averageRating: 0,
+  oneStar: 0,
+  twoStar: 0,
+  threeStar: 0,
+  fourStar: 0,
+  fiveStar: 0,
+  currentRating: 0,
+  currentUserRating: null
+}
+
+const RatingComp: React.FunctionComponent<IProps> = ({ programid }) => {
+  console.log(programid)
+  const [currentRating, setCurrentRating] = useState<number>(0);
+  const [programRating, setProgramRating] = useState(programRatingTemplate);
+  const [refreshRating, setRefreshRating] = useState<boolean>(false);
+  const ratingBars = ['oneStar', 'twoStar', 'threeStar', 'fourStar', 'fiveStar'];
+
+  useEffect(() => {
+    axios.get(`https://api.microlearning.ballisticlearning.com/learning-service/api/v1/public/rating/${programid}`).then((res: any) => {
+      console.log(res)
+      if (res.status === 200 && res.data.averageRating !== undefined) {
+        setProgramRating(res.data);
+        setCurrentRating(res.data.currentRating);
+      }
+    }).catch((err: any) => {
+        console.log(err);
+    });
+  }, [refreshRating, programid]);
+
+  return (
+    <React.Fragment>
+      <div className="po-section reviewrating-step mt-5">
+        <h5 id="po-reviewrating">Review and Rating</h5>
+        <Row className="mt-4">
+          <Col md="2" className="text-center por-left">
+            <h2 className="rating-count">{`${programRating.averageRating.toFixed(2)}`}</h2>
+            <StarRating
+              totalStars={5}
+              currentRating={roundToWhole(programRating.averageRating)}
+            />
+          </Col>
+          <Col md="10" className=" por-right">
+            {ratingBars.map((elem, index) => (
+              <div className="mb-2 d-flex align-items-center" key={index}>
+                <div
+                  className="progress w-25"
+                  role="progressbar"
+                  aria-label="Basic example"
+                >
+                  <div
+                    className="progress-bar"
+                    id={elem}
+                    style={{ width: `${Math.floor(programRating[elem])}%` }}
+                  ></div>
+                </div>
+                <StarRating
+                  totalStars={5}
+                  currentRating={index + 1}
+                />
+                <strong className="ms-2">{programRating[elem] > 0 ? `${Math.floor(programRating[elem])}%` : '0%'}</strong>
+              </div>
+            ))}
+          </Col>
+        </Row>
+        <Review />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default RatingComp;
--- src/pages/home/programSummary.tsx
+++ src/pages/home/programSummary.tsx
@@ -2,99 +2,211 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { pagination } from "../../utils/pagination";
 import { useParams } from "react-router-dom";
-import Footer from "../newFooter";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/circlelogo-blue.svg";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row, Image } from "react-bootstrap";
+import ProgramInstructors from "./preview/instructors";
+import PageTitle from "../../widgets/pageTitle";
+import programImage from "../../assets/images/course-default.jpg";
+import RatingComp from "./preview/ratings/ratings";
+// import Footer from "../newFooter";
+import "../newHeader/style.scss";
+import ProgramDefaultImg from "../../assets/images/course-default.jpg"
+import Curriculum from "./preview/curriculum";
 
-function ProgramSummary() {
+// import RatingComp from "./ratings/ratings";
+
+interface ICurrentProgram {
+  data: [];
+  status: boolean;
+  id: string | undefined;
+}
+
+const ProgramSummary = () => {
+  const { Programid } = useParams();
+  const [instructors, setInstructors] = useState([]);
+  const [currentProgram, setCurrentProgram] = useState<ICurrentProgram>({
+    data: [],
+    status: false,
+    id: Programid,
+  });
   const [filterUpdate, setFilterUpdate] = useState<any>({
     pageNumber: 0,
     pageSize: pagination.PERPAGE,
   });
-  const [programs, setPrograms] = useState([]);
-  const { Programid } = useParams();
 
   useEffect(() => {
     axios
       .get(
-        `https://api.microlearning.ballisticlearning.com/learning-service/api/v1/public/programs?pageNumber=${filterUpdate.pageNumber}&pageSize=${filterUpdate.pageSize}`
+        `https://api.microlearning.ballisticlearning.com/learning-service/api/v1/public/programs?pageNumber=${filterUpdate.pageNumber}&pageSize=${filterUpdate.pageSize}&Id=${Programid}`
       )
-      .then((result: any) => {
-        if (result.data !== "" && result.status === 200) {
-          const filteredData = result.data.items.filter(
-            (item: any) => item.id == Programid
-          );
-          setPrograms(filteredData);
+      .then((res: any) => {
+        if (res.data !== "" && res.status === 200) {
+          setCurrentProgram({
+            data: res.data.items,
+            status: true,
+            id: Programid,
+          });
+          setInstructors(res.data.instructors);
         }
       })
       .catch((err: any) => {
         console.log(err);
       });
-  }, [filterUpdate]);
+  }, []);
+
+  const previewMetafields = (metaData: Array<any>) => {
+    return (
+      <>
+        {metaData.map((el: any, index: number) => (
+          <div className="mt-5" key={index}>
+            <h5>{el.title}</h5>
+            <div dangerouslySetInnerHTML={{ __html: el.description }} />
+          </div>
+        ))}
+      </>
+    );
+  };
+
+  const previewTagfields = (metaData: Array<any>) => {
+    return (
+      <>
+        <strong>Tags</strong>
+        {metaData.map((el: any, index: number) => (
+          <ul key={index}>
+            <li>{el.name},</li>
+          </ul>
+        ))}
+      </>
+    );
+  };
 
   return (
     <>
-     <div className="landing-wrapper programlist-wrapper">
-      <div className="landing-header program-summary">
-        <div className="d-flex justify-content-between align-items-center">
-          <div className="logo-wrapper">
-            <Link to="/">
-              <img src={logo} alt="logo" className="img img-fluid" />
-            </Link>
-          </div>
-        </div>
-      </div>
-  <Container fluid>
-        {programs.map((program: any) => (
-          <div key={program.id} className="card1 bg-white p-2 rounded shadow">
-       <div className="d-flex gap-3 flex-wrap">
-       <img src={program.files[0].url} alt={program.name} className="img img-fluid rounded" style={{width:"500px"}}/>
-             <div className="d-flex flex-column">
-             <h5 className="card-title">{program.name}</h5>
-              <p
-                className="card-text"
-                dangerouslySetInnerHTML={{ __html: program.description }}
-              ></p> 
-             </div>
-       </div>
-            <div className="">
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <strong>Program Code:</strong> {program.programCode}
-                </li>
-                <li className="list-group-item">
-                  <strong>Mode of Study:</strong> {program.modeOfStudy}
-                </li>
-                <li className="list-group-item">
-                  <strong>Batch Year:</strong> {program.batchYear}
-                </li>
-                <li className="list-group-item">
-                  <strong>Full Lifetime Access:</strong>{" "}
-                  {program.fullLifeTimeAccess ? "Yes" : "No"}
-                </li>
-                <li className="list-group-item">
-                  <strong>Duration:</strong> {program.durationValue}{" "}
-                  {program.durationUnit}
-                </li>
-                <li className="list-group-item">
-                  <strong>Program Type:</strong> {program.programTypeName}
-                </li>
-                <li className="list-group-item">
-                  <strong>Department Name:</strong> {program.departmentName}
-                </li>
-                <li className="list-group-item">
-                  <strong>Discipline Name:</strong> {program.disciplineName}
-                </li>
-              </ul>
+      <div className="landing-wrapper programlist-wrapper">
+        <div className="landing-header program-summary">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="logo-wrapper">
+              <Link to="/">
+                <img src={logo} alt="logo" className="img img-fluid" />
+              </Link>
             </div>
           </div>
-        ))}
-  </Container>
-  </div>
+        </div>
+        <Container fluid>
+          <PageTitle pageTitle={``} gobacklink="/programlist" />
+          {currentProgram?.data?.map((el: any) => (
+            <div className="program-overview-container" key={el.id}>
+              <Row>
+                <Col md={3}>
+                  <Image
+                    // src={el.files.length > 0 ? el.files[0].url : programImage}
+                    src={
+                      el.files && el.files.length > 0
+                        ? el.files[0].url
+                        : ProgramDefaultImg
+                    }
+                    alt={el.name}
+                    fluid
+                    rounded
+                  />
+                </Col>
+                <Col md={9}>
+                  <h5 className="program-title">
+                    {el.name} <span></span> {el.disciplineName}
+                  </h5>
+                  {/* <div dangerouslySetInnerHTML={{ __html: el.description }} /> */}
+                  <div className="key-information">
+                    <strong>Key Information</strong>
+                    <ul>
+                      <li>
+                        Duration:{" "}
+                        <strong>
+                          {el.durationValue} {el.durationUnit}
+                        </strong>
+                      </li>
+                      <li>
+                        Program Code: <strong>{el.programCode}</strong>
+                      </li>
+                      <li>
+                        Department: <strong>{el.departmentName}</strong>
+                      </li>
+                      <li>
+                        Program Type: <strong>{el.programTypeName}</strong>
+                      </li>
+                      <li>
+                        Batch Year: <strong>{el.batchYear}</strong>
+                      </li>
+                      <li>
+                        Mode of Study: <strong>{el.modeOfStudy}</strong>
+                      </li>
+                      <li>
+                        Lifetime Access:{" "}
+                        <strong>{el.fullLifeTimeAccess ? "Yes" : "No"}</strong>
+                      </li>
+                      <li className="d-none">
+                        Published:{" "}
+                        <strong>{el.published ? "Yes" : "No"}</strong>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+              </Row>
+              <div className="po-tab-container mb-3">
+                <div
+                  id="tabStep-indicator"
+                  className="tabStep-indicator sticky-top"
+                >
+                  <a href="#po-objective" className="step">
+                    Objective
+                  </a>
+                  <a href="#po-curriculum" className="step">
+                    Curriculum
+                  </a>
+                  <a href="#po-instructor" className="step">
+                    Instructor
+                  </a>
+                  <a href="#po-reviewrating" className="step">
+                    Review and Rating
+                  </a>
+                </div>
+
+                <div
+                  data-bs-spy="scroll"
+                  data-bs-target="#tabStep-indicator"
+                  data-bs-offset="0"
+                  tabIndex={0}
+                ></div>
+
+                <div className="po-section objective-step mt-5">
+                  <h5 id="po-objective">Objective</h5>
+                  <div dangerouslySetInnerHTML={{ __html: el.objective }} />
+                  {el.metaFields.length > 0
+                    ? previewMetafields(el.metaFields)
+                    : ""}
+                </div>
+                  {/* Curriculum component */}
+                  <Curriculum
+                      programId={el.id}
+                    />
+
+                <div className="po-section instructor-step mt-5">
+                  <ProgramInstructors instructorsData={instructors} />
+                </div>
+                <RatingComp programid={Programid} />
+                <div className="program-tags mt-5">
+                    {el.tags.length > 0
+                      ? previewTagfields(el.tags)
+                      : ""}
+                  </div>
+              </div>
+            </div>
+          ))}
+        </Container>
       {/* <Footer/> */}
+      </div>
     </>
   );
-}
+};
 
 export default ProgramSummary;
