328b0553180829848801cfe3e555cd7c7984bee0
alkama.azim <alkama.azim@ballisticlearning.com>
2024-03-15 15:18:19 +0530

    apply congig APIs in components

--- src/pages/adminSiteConfiguration/index.tsx
+++ src/pages/adminSiteConfiguration/index.tsx
@@ -1,66 +1,79 @@
+import Swal from "sweetalert2";
 import Header from "../newHeader";
 import Footer from "../newFooter";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
-import { Formik, Form, Field } from "formik";
 import HeaderTabs from "../headerTabs";
+import "sweetalert2/src/sweetalert2.scss";
 import { Container } from "react-bootstrap";
+import { Formik, Form, Field } from "formik";
 import PageTitle from "../../widgets/pageTitle";
 import React, { useState, useEffect } from "react";
 import CkEditor from "../../widgets/editor/CKEditor";
 import BreadcrumbComponent from "../../widgets/breadcrumb";
+import FieldLabel from "../../widgets/formInputFields/labels";
 import { getData, postData } from "../../adapters/coreservices";
 import CustomButton from "../../widgets/formInputFields/buttons";
+import { LoadingButton } from "../../widgets/formInputFields/buttons";
+
+type ConfigItem = {
+  tab: string;
+  type: string;
+  config: string;
+};
 
 type Props = {};
 
-const initialValues = {
-  configKey: "",
-  configValue: ""
-};
-
 const AdminSiteConfiguration = (props: Props) => {
-  const [keyTitle, setKeyTitle] = useState({});
-  const [mailConfigData, setMailConfigData] = useState([]);
-  const [tabTitles, setTabTitles] = useState<any>([]);
+  const [mailConfigData, setMailConfigData] = useState<ConfigItem[]>([]);
+  const [tabTitles, setTabTitles] = useState<string[]>([]);
 
-  // call API to get config data === >>>
   useEffect(() => {
     let endPoint = `/config`;
     getData(endPoint, {}).then((res: any) => {
       if (res.data !== "" && res.status === 200) {
         setMailConfigData(res.data);
         const tabTitlesList = [
-          ...new Set(res.data.map((item) => item.tabTitle)),
+          ...new Set(res.data.map((item: ConfigItem) => item.tab)),
         ];
         setTabTitles(tabTitlesList);
       }
     });
   }, []);
 
-  useEffect(() => {
-    let x = {};
-    tabTitles.forEach((item: any) => {
-      let y = [];
-      mailConfigData.forEach((el: any) => {
-        if (el.tabTitle === item) {
-          y.push(el);
-        }
-        x[item] = y;
-      });
-    });
-    setKeyTitle(x);
-  }, [tabTitles, mailConfigData]);
-
-  const handleFormSubmit = (values: any, { setSubmitting, resetForm }: any) => {
-    postData('config/save', values).then((res: any) => {
+  const handleFormSubmit = (
+    values: any,
+    { setSubmitting, resetForm }: any,
+    tabTitle: any
+  ) => {
+    let newValue = [
+      { configKey: values.config_1, configValue: values.subject },
+      {
+        configKey: values.config_0,
+        configValue: values.description,
+      },
+    ];
+    setSubmitting(true)
+    postData("config/save", newValue).then((res: any) => {
       if (res.data !== "" && res.status === 200) {
-        console.log(res)
+        setSubmitting(false)
+        Swal.fire({
+          timer: 3000,
+          width: "25em",
+          color: "#666",
+          icon: "success",
+          background: "#e7eef5",
+          showConfirmButton: false,
+          text: "Mail has been successfully added."
+        });
+        resetForm();
       }
-    });
-  }
+    }).catch((error) => {
+      console.log(error)
+    })
+  };
 
   return (
     <React.Fragment>
@@ -79,71 +92,114 @@ const AdminSiteConfiguration = (props: Props) => {
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
               <Row>
                 {tabTitles.length > 0 &&
-                  tabTitles.map((tab: any, index: number) =>
-                    Object.entries(keyTitle).map(
-                      ([Key, Value]) =>
-                        Key === tab && (
-                          <div key={index}>
-                            <Col sm={3}>
-                              <Nav variant="pills" className="flex-column">
-                                <Nav.Item>
-                                  <Nav.Link eventKey={index}>{tab}</Nav.Link>
-                                </Nav.Item>
-                              </Nav>
-                            </Col>
-                            <Col sm={9}>
-                              <Tab.Content>
-                                <Tab.Pane eventKey={index}>
-                                  <Formik
-                                    initialValues={initialValues}
-                                    // validationSchema={queryFormSchema}
-                                    onSubmit={(values, action) => {
-                                      handleFormSubmit(values, action);
-                                      // console.log(values)
-                                    }}
-                                  >
-                                    {({
-                                      errors,
-                                      touched,
-                                      isSubmitting,
-                                      setFieldValue,
-                                      handleChange
-                                    }) => (
-                                      <Form>
-                                        <div className="d-flex flex-column">
-                                          {Value.map(
-                                            (item) => item.type
-                                          ).includes("textfield") && (
-                                            <Field type="text" name="configKey" placeholder="Subject" />
+                  tabTitles.map((tabTitle: string, index: number) => (
+                    <React.Fragment key={index}>
+                      <Col sm={3}>
+                        <Nav variant="pills" className="flex-column">
+                          <Nav.Item>
+                            <Nav.Link eventKey={index}>{tabTitle}</Nav.Link>
+                          </Nav.Item>
+                        </Nav>
+                      </Col>
+                      <Col sm={9}>
+                        <Tab.Content>
+                          <Tab.Pane eventKey={index}>
+                            <Formik
+                              initialValues={{
+                                subject: "",
+                                description: "",
+                                ...mailConfigData
+                                  .filter((item) => item.tab === tabTitle)
+                                  .reduce((acc, item, index) => {
+                                    acc[`config_${index}`] = item.config;
+                                    return acc;
+                                  }, {}),
+                              }}
+                              onSubmit={(values, action) => {
+                                handleFormSubmit(values, action, tabTitle);
+                              }}
+                            >
+                              {({ isSubmitting, handleChange }) => (
+                                <Form>
+                                  <div className="d-flex flex-column">
+                                    {mailConfigData
+                                      .filter((item) => item.tab === tabTitle)
+                                      .sort((a, b) => {
+                                        if (a.type === "textfield" && b.type === "textarea") {
+                                          return -1;
+                                        } 
+                                        else if (a.type === "textarea" && b.type === "textfield") {
+                                          return 1;
+                                        }
+                                        return 0;
+                                      })
+                                      .map((item, index) => (
+                                        <React.Fragment key={index}>
+                                          <Field
+                                            type="text"
+                                            name={`config_${index}`}
+                                            hidden
+                                          />
+                                          {item.type === "textfield" && (
+                                            <>
+                                              <FieldLabel
+                                                htmlfor="subject"
+                                                labelText={item.label}
+                                                // required="required"
+                                              />
+                                              <Field
+                                                type="text"
+                                                name="subject"
+                                                placeholder="Subject"
+                                              />
+                                            </>
                                           )}
-                                          {Value.map(
-                                            (item) => item.type
-                                          ).includes("textarea") && (
-                                            <CkEditor name="configValue" handleChange={handleChange} />
+                                          {item.type === "textarea" && (
+                                            <>
+                                              <FieldLabel
+                                                htmlfor="description"
+                                                labelText={item.label}
+                                                // required="required"
+                                              />
+                                              <CkEditor
+                                                name="description"
+                                                handleChange={handleChange}
+                                              />
+                                            </>
                                           )}
-                                          <div>
-                                            <CustomButton
-                                              type="submit"
-                                              variant="primary"
-                                              btnText="Submit"
-                                            />{" "}
-                                            <CustomButton
-                                              type="reset"
-                                              btnText="Reset"
-                                              variant="outline-secondary"
-                                            />
-                                          </div>
-                                        </div>
-                                      </Form>
+                                        </React.Fragment>
+                                      ))}
+
+                                    {isSubmitting === false ? (
+                                      <div className="modal-buttons">
+                                        <CustomButton
+                                          type="submit"
+                                          variant="primary"
+                                          isSubmitting={isSubmitting}
+                                          btnText="Submit"
+                                        />{" "}
+                                        <CustomButton
+                                          type="reset"
+                                          btnText="Reset"
+                                          variant="outline-secondary"
+                                        />
+                                      </div>
+                                    ) : (
+                                      <LoadingButton
+                                        variant="primary"
+                                        btnText="Submitting..."
+                                        className="modal-buttons"
+                                      />
                                     )}
-                                  </Formik>
-                                </Tab.Pane>
-                              </Tab.Content>
-                            </Col>
-                          </div>
-                        )
-                    )
-                  )}
+                                  </div>
+                                </Form>
+                              )}
+                            </Formik>
+                          </Tab.Pane>
+                        </Tab.Content>
+                      </Col>
+                    </React.Fragment>
+                  ))}
               </Row>
             </Tab.Container>
           </Container>
