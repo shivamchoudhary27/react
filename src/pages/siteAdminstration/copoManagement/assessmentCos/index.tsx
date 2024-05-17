8bfe13182166366f90c51c8d86ec514f31fdeaf6
krisha.singh <krisha.singh@ballisticlearning.com>
2024-05-17 13:20:04 +0530

    Integrate api in co po assessment

--- src/pages/siteAdminstration/copoManagement/assessmentCos/index.tsx
+++ src/pages/siteAdminstration/copoManagement/assessmentCos/index.tsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import AssessmentTable from "./assessmentTable";
 import { Alert } from "react-bootstrap";
-
+import { getData } from "../../../../adapters/microservices";
+import { useParams } from "react-router-dom";
 type Props = {
   setActiveTab: any;
 };
 
 const AssessmentForCOs = (props: Props) => {
+  const { cid } = useParams();
+  const [apiStatus, setApiStatus] = useState("");
+  const [refreshData, setRefreshData] = useState<boolean>(true);
+  const [assessmentMappingData, setAssessmentMappingData] = useState([])
+  const [initialValues, setInitialValue] = useState([]);
+
+
+useEffect(() => {
+  setApiStatus("started");
+  getData(`/${cid}/assessment/mapping`, {})
+  .then((res: any) => {
+    if (res.data !== "" && res.status === 200) {
+      setAssessmentMappingData(res.data)
+      setInitialValue(res.data);
+    }
+    setApiStatus("finished");
+  })
+  .catch((err: any) => {
+    setApiStatus("finished");
+    console.log(err);
+  });
+}, [])
+
+const refreshToggle = () => {
+  setRefreshData(!refreshData);
+};
+
   return (
     <div>
-      <AssessmentTable setActiveTab={props.setActiveTab} />
+      <AssessmentTable 
+        setActiveTab={props.setActiveTab} 
+        assessmentData={assessmentMappingData}
+        refreshToggle={refreshToggle}
+        initialValues={initialValues}
+        setInitialValue={setInitialValue}
+         />
       <Alert variant="primary" className="mt-4">
         <strong>Note:</strong>
         <ul>
